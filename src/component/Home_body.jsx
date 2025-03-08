245aead9e59b79b85f63f7e2d8d0dd1c8bfd2d9b
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-03-08 15:13:17 +0530

    Home Screen Body UI Implementation

--- src/component/Home_body.jsx
+++ src/component/Home_body.jsx
@@ -16,16 +16,31 @@ var HomeBody = () => {
               <div className="home-body-view-info-container-skills">
                 <Typical
                   steps={[
-                    "Flutter Developer", 
-                    1000, 
-                    "Mobile Developer", 
+                    "Flutter Developer",
                     1000,
-                    "Front End Developer", 
-                    1000
-                    ]}
+                    "Mobile Developer",
+                    1000,
+                    "Front End Developer",
+                    1000,
+                  ]}
                   loop={Infinity}
                   wrapper="p"
                 />
+              </div>
+              <p className="home-body-view-info-container-desc">
+                I am a Flutter Developer with over 2 years of experience in building
+                scalable B2B and B2C applications for Android, iOS, and Web
+              </p>
+              <div className="home-body-view-info-container-social-links">
+              <a href="https://github.com/Karanjit08" target="_blank">
+              <img src="https://img.icons8.com/doodle/40/000000/github--v1.png"></img>
+              </a>
+              <a href="https://www.linkedin.com/in/karanjit-singh-randhawa-55bb1b1b8/" target="_blank">
+              <img src="https://img.icons8.com/doodle/40/000000/linkedin--v2.png"></img>
+              </a>
+              <a href="https://www.instagram.com/karanjit_randhawa99/" target="_blank">
+              <img src="https://img.icons8.com/doodle/40/000000/instagram-new--v2.png"></img>
+              </a>
               </div>
             </div>
           </div>
